c0076f91a7ac2e7ebb1dc83af1d5f55c7df7a253
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-20 13:09:54 +0900

    Change link color if passname matches

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,27 @@
+import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { SPACING } from "../constants/styles";
 
 const headerStyle = {
   padding: SPACING,
 } as const;
 
-const linkStyle = {
-  marginRight: 15,
-} as const;
-
 const LinkTab: React.FC<{ href: string; title: string }> = ({
   href,
   title,
 }) => {
+  const router = useRouter();
+  const style = React.useMemo(
+    () => ({
+      marginRight: 15,
+      color: router.pathname === href ? "black" : "gray",
+    }),
+    [href]
+  );
   return (
     <Link href={href}>
-      <a style={linkStyle}>{title}</a>
+      <a style={style}>{title}</a>
     </Link>
   );
 };
