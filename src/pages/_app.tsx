18b8c4f30a15d7ce1934b4c75b4d41ab955cea0d
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-20 13:57:15 +0900

    Change _app.tsx to function component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,20 @@
 import React from "react";
-import App, { Container } from "next/app";
+import { Container, AppProps } from "next/app";
 import Header from "../components/Header";
 import Body from "../components/Body";
 import Content from "../components/Content";
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <Container>
-        <Body>
-          <Header />
-          <Content>
-            <Component {...pageProps} />
-          </Content>
-        </Body>
-      </Container>
-    );
-  }
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Container>
+      <Body>
+        <Header />
+        <Content>
+          <Component {...pageProps} />
+        </Content>
+      </Body>
+    </Container>
+  );
+};
 
 export default MyApp;
