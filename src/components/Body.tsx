7416b62c2706e684b3e3742d983e054b3635739a
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-20 12:48:53 +0900

    Add global style

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -4,7 +4,26 @@ const layoutStyle = {
 };
 
 const Component: React.FC<{}> = ({ children }) => (
-  <div style={layoutStyle}>{children}</div>
+  <div style={layoutStyle}>
+    {children}
+    <style jsx global>{`
+      body {
+        margin: 0;
+        padding: 0;
+        font-size: 18px;
+        font-weight: 400;
+        line-height: 1.8;
+        color: #333;
+        font-family: sans-serif;
+      }
+      h1 {
+        font-weight: 700;
+      }
+      p {
+        margin-bottom: 10px;
+      }
+    `}</style>
+  </div>
 );
 
 export default Component;
