ac4febcc1e554f99982489d362cbc8b81d934036
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-21 14:23:53 +0900

    Remove Container from _app.tx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { Container, AppProps } from "next/app";
+import { AppProps } from "next/app";
 import Wrapper from "../components/Wrapper";
 import Header from "../components/Header";
 import Content from "../components/Content";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Container>
-      <Wrapper>
-        <Header />
-        <Content>
-          <Component {...pageProps} />
-        </Content>
-      </Wrapper>
-    </Container>
+    <Wrapper>
+      <Header />
+      <Content>
+        <Component {...pageProps} />
+      </Content>
+    </Wrapper>
   );
 };
 
