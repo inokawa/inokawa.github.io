ee344178d447682366806ddf2a9b863807e96fab
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-05-01 01:11:34 +0900

    Remove frontmatter from html

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -7,11 +7,15 @@ import html from "rehype-stringify";
 // @ts-ignore
 import slug from "remark-slug";
 import contents from "remark-contents";
+import frontmatter from "remark-frontmatter";
 import matter, { GrayMatterFile } from "gray-matter";
+import filter from "./unistFilter";
 
 const processor = unified()
   .use(markdown, { commonmark: true })
+  .use(frontmatter, ["yaml", "toml"])
   .use(slug)
+  .use(filter, ["yaml", "toml"])
   .use(remark2rehype)
   .use(html)
   .freeze();
