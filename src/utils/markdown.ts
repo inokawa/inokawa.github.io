2e50d9d4d0265e52d55028c48b01e32cc50bbb02
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-05-02 11:56:26 +0900

    Remove rehype-stringify

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -5,28 +5,12 @@ import remark2rehype from "remark-rehype";
 // @ts-ignore
 import rehype2react from "rehype-react";
 // @ts-ignore
-import html from "rehype-stringify";
-// @ts-ignore
 import slug from "remark-slug";
 import contents from "remark-contents";
 import frontmatter from "remark-frontmatter";
 import matter, { GrayMatterFile } from "gray-matter";
 import React from "react";
 import filter from "./unistFilter";
-
-const processor = unified()
-  .use(markdown, { commonmark: true })
-  .use(frontmatter, ["yaml", "toml"])
-  .use(slug)
-  .use(filter, ["yaml", "toml"])
-  .use(remark2rehype)
-  .use(html)
-  .freeze();
-
-export const createHtml = async (input: string): Promise<string> => {
-  const data = await processor().process(input);
-  return data.contents as string;
-};
 
 export const createReactElement = (input: string): React.ReactElement => {
   const processor = unified()
