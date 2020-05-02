dff5ada409d486dbba0a57c985fd9a6b732b10c0
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-05-02 18:50:10 +0900

    Update font-size

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,7 +26,7 @@ export default class MyDocument extends Document {
             body {
               margin: 0;
               padding: 0;
-              font-size: 18px;
+              font-size: 16px;
               font-weight: 400;
               line-height: 1.8;
               color: ${COLOR.BLACK};
