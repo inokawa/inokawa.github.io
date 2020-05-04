905178402c350c19a49ceb49a2a798c059f36ff7
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-05-05 02:23:14 +0900

    Use throttle for useScrollSpy hook

--- src/hooks/useScrollSpy.ts
+++ src/hooks/useScrollSpy.ts
@@ -1,25 +1,28 @@
 import React from "react";
+import { throttle } from "../utils/timer";
 
 export const useScrollSpy = (ids: string[]): string => {
   const [section, setSection] = React.useState<string>("");
 
-  // TODO debounce
-  const spyScroll = React.useCallback(() => {
-    ids.some((id, i) => {
-      const elem = document.getElementById(id);
-      const nextElem = document.getElementById(ids[i + 1]);
-      if (!elem) {
-        return false;
-      }
-      if (
-        window.scrollY > elem.offsetTop &&
-        (!nextElem || window.scrollY < nextElem.offsetTop)
-      ) {
-        setSection(id);
-        return true;
-      }
-    });
-  }, [ids]);
+  const spyScroll = React.useCallback(
+    throttle(() => {
+      ids.some((id, i) => {
+        const elem = document.getElementById(id);
+        const nextElem = document.getElementById(ids[i + 1]);
+        if (!elem) {
+          return false;
+        }
+        if (
+          window.scrollY > elem.offsetTop &&
+          (!nextElem || window.scrollY < nextElem.offsetTop)
+        ) {
+          setSection(id);
+          return true;
+        }
+      });
+    }, 100),
+    [ids]
+  );
 
   React.useEffect(() => {
     window.addEventListener("scroll", spyScroll);
