007b89703bd89c3baebb327aa89ca80e6a092dfe
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-05-04 20:59:18 +0900

    Replace style object with styled jsx

--- src/components/ArticleWrapper.tsx
+++ src/components/ArticleWrapper.tsx
@@ -5,19 +5,24 @@ import {
   SPACING,
 } from "../constants/styles";
 
-const contentStyle = {
-  flex: 1,
-  border: `solid ${BORDER_WIDTH}px ${COLOR_GRAY}`,
-  borderRadius: BORDER_RADIUS,
-  paddingTop: SPACING * 4,
-  paddingBottom: SPACING * 4,
-  paddingLeft: SPACING * 6,
-  paddingRight: SPACING * 6,
-  maxWidth: 800,
-} as const;
-
 const Component: React.FC<{}> = ({ children }) => (
-  <div style={contentStyle}>{children}</div>
+  <div>
+    {children}
+    <style jsx>
+      {`
+        div {
+          flex: 1;
+          border: solid ${BORDER_WIDTH}px ${COLOR_GRAY};
+          border-radius: ${BORDER_RADIUS}px;
+          padding-top: ${SPACING * 4}px;
+          padding-bottom: ${SPACING * 4}px;
+          padding-left: ${SPACING * 6}px;
+          padding-right: ${SPACING * 6}px;
+          max-width: 800px;
+        }
+      `}
+    </style>
+  </div>
 );
 
 export default Component;
