994a7799dd283bd9ef52620a1cdfe9e88fdfeaa4
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-05-05 16:14:14 +0900

    Update style of Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,44 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { SPACING, COLOR } from "../../constants/styles";
+import { SPACING, COLOR, BORDER_RADIUS } from "../../constants/styles";
 
 const LinkTab: React.FC<{ href: string; title: string }> = ({
   href,
   title,
 }) => {
   const router = useRouter();
-  const style = React.useMemo(
-    () => ({
-      marginRight: 15,
-      color: router.pathname === href ? COLOR.BLACK : COLOR.GRAY,
-    }),
+  const className = React.useMemo(
+    () =>
+      (
+        href === "/"
+          ? router.pathname === href
+          : router.pathname.indexOf(href) === 0
+      )
+        ? "selected"
+        : undefined,
     [href, router.pathname]
   );
+
   return (
     <Link href={href}>
-      <a style={style}>{title}</a>
+      <a className={className}>
+        {title}
+        <style jsx>{`
+          a {
+            color: ${COLOR.GRAY};
+            margin-right: 15px;
+            padding: ${SPACING / 2}px;
+            border-radius: ${BORDER_RADIUS}px;
+
+            transition: all 0.1s ease-in-out;
+          }
+          a.selected,
+          a:hover {
+            color: ${COLOR.PRIMARY};
+          }
+        `}</style>
+      </a>
     </Link>
   );
 };
@@ -38,7 +59,11 @@ const Component: React.FC<{}> = () => {
             display: flex;
             padding-top: ${SPACING * 2}px;
             padding-bottom: ${SPACING * 2}px;
-            background: linear-gradient(to top,rgba(255,255,255,0),#fff 50%);
+            background: linear-gradient(
+              to top,
+              rgba(255, 255, 255, 0),
+              #fff 50%
+            );
           }
           .tab-area {
             display: flex;
