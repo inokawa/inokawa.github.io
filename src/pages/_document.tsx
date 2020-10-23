0b8f5686785e2bb12c368dbc24f7dc860c2a8bef
inokawa <48897392+inokawa@users.noreply.github.com>
2020-10-23 20:04:06 +0900

    Add title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,6 +22,11 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>inokawa.github.io</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
           <style>{`
             body {
               margin: 0;
