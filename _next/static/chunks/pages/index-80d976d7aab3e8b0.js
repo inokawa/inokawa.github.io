44b2af7476968565db37a660864af992b428153d
inokawa <inokawa@users.noreply.github.com>
2022-10-30 03:18:16 +0000

    deploy: 6276347c9ae20fab6bcf721c106c5d6b23601225

---
+++ _next/static/chunks/pages/index-80d976d7aab3e8b0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(5970)}])},5970:function(n,u,t){"use strict";t.r(u);var _=t(5893);let e=()=>(0,_.jsx)(_.Fragment,{children:(0,_.jsx)("div",{})});u.default=e}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
